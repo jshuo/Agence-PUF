7bad73a8128240c9e0fddc3ee01c9ffb29a77dbe
Jeff Shuo <jeffshuo@secuxtech.com>
2023-05-03 14:27:47 +0800

    added Oracle Entroy Callback implementation (WIP)

--- lottery/contracts/Lottery.sol
+++ lottery/contracts/Lottery.sol
@@ -1,50 +1,55 @@
-// SPDX-License-Identifier: MIT
+interface Oracle {
+    event RandomRequested(address indexed requester, uint256 indexed id);
+    event RandomIssued(uint256 indexed id, uint256 entropy);
+    function getEntropy(uint256 id) external view returns (address, uint256);
+    function requestRandom(address contractAddress) external returns (uint256);
+}
 
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 import "./RandomConsumerBase.sol";
 
 contract Lottery is RandomConsumerBase {
+    event RandomReceived(uint256 requestId, uint256 entropy);
+    uint256 public currentRequestId = 0;
+    uint256 public currentEntropy = 0;
+    address public manager;
+    address payable[] public players;
+    Oracle public oracle = Oracle(0x0000000000000000000000000000000000000801); // Add Oracle contract address as a constant
 
+    constructor() {
+        manager = msg.sender;
+    }
 
-        
-        event RandomReceived(uint256 requestId, uint256 entropy);
-        uint256 public currentRequestId = 0;
-        uint256 public currentEntropy = 0;
-        address public manager;
-        address payable[] public players;
-        
-        constructor() {
-            manager = msg.sender;
-        }
+    function enter() public payable {
+        require(msg.value > .005 ether);
+        players.push(payable(msg.sender));
+    }
 
-        function enter() public payable {
-            require(msg.value > .005 ether);
-            players.push(payable(msg.sender));
-        }
-        
-        function random() private view returns (uint) {
-            return uint(keccak256(abi.encodePacked(block.prevrandao, block.timestamp, players)));
-        }
-        function pickWinner() public restricted {
-            uint index = random() % players.length;
-            players[index].transfer(address(this).balance);
-            players = new address payable[](0);
-            emit RandomReceived(random() % 1000,random() % 1000);
-        }
-        
-        modifier restricted() {
-            require(msg.sender == manager);
-            _;
-        }
-        
-        function getPlayers() public view returns (address payable[] memory) {
-            return players;
-        }        
-        function executeImpl(uint256 requestId, uint256 entropy) internal virtual override  {
-            //my code
-            currentRequestId = requestId;
-            currentEntropy = entropy;
-            emit RandomReceived(requestId,entropy);
-        }
+    // function random() private view returns (uint) {
+    //     return uint(keccak256(abi.encodePacked(block.prevrandao, block.timestamp, players)));
+    // }
+
+    function pickWinner() public restricted {
+        uint256 requestId = oracle.requestRandom(address(this)); // Call requestRandom from the Oracle contract
+        currentRequestId = requestId;
+    }
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    function getPlayers() public view returns (address payable[] memory) {
+        return players;
+    }
+
+    function executeImpl(uint256 requestId, uint256 entropy) internal virtual override {
+        uint index = entropy % players.length;
+        players[index].transfer(address(this).balance);
+        players = new address payable[](0);
+        currentRequestId = requestId;
+        currentEntropy = entropy;
+        emit RandomReceived(requestId, entropy);
+    }
 }
-
--- lottery/test/Lottery.test.js
+++ lottery/test/Lottery.test.js
@@ -117,7 +117,7 @@ const abi = [
     signature: '0xf71d96cb',
   },
 ];
-let lottery = ''
+let lottery = '';
 
 const provider = new HDWalletProvider(
   'flip february broom truck razor guard enter rebuild click return impulse census imitate sense news cruise swift cat response view cover evoke raw time',
@@ -126,12 +126,63 @@ const provider = new HDWalletProvider(
   // remember to change this to your own endpoint!
 );
 const web3 = new Web3(provider);
+// Separate WebSocketProvider for subscriptions
+const wsProvider = new Web3.providers.WebsocketProvider(
+  'wss://sepolia.infura.io/ws/v3/e9f62e559d264acca5fe498412c1d9b9'
+  // remember to change this to your own WebSocket endpoint!
+);
+
+// Set the provider for subscriptions separately
+web3.eth.subscribe = (type, options, callback) => {
+  const subscription = new Web3(wsProvider).eth.subscribe(
+    type,
+    options,
+    callback
+  );
+  return subscription;
+};
+
+// Type of event to listen for, in this case, logs (smart contract events)
+const eventType = 'logs';
+
+// Options (needed for the 'logs' event type)
+const options = {
+  address: '0x5a0A5Ba73f64D83842c52a7398eE414d14701f19', // Replace with your smart contract address
+  topics: [
+    '0xd26ff88a1db9b3b7e9a6a7cd0abec5d2c8efce0a95a30bf024b29e7365f81f0d',
+  ], // Replace with an array of topics if needed, or use [null] to listen for all events from the specified address
+};
+
+function extractHexToDecimal(hexString, startPosition) {
+  // extract remaining hex string from start position to end of string
+  const remainingHexString = hexString.substring(startPosition);
+  console.log(remainingHexString);
+
+  // convert hex string to decimal number
+  const decimalNumber = parseInt(remainingHexString, 16);
+
+  return decimalNumber;
+}
+// Callback function to handle the event
+const eventCallback = (error, result) => {
+  if (error) {
+    console.error('Error in subscription:', error);
+    return;
+  }
+
+  // Handle the result (in this case, the log data)
+  const startPosition = 32 * 2 + 2 + 1; //including 0x
+
+  const decimalNumber = extractHexToDecimal(result.data, startPosition);
+  console.log('Random Number is:', decimalNumber);
+};
 
 let accounts;
 
-beforeEach(async () => {
+before(async () => {
   accounts = await web3.eth.getAccounts();
   lottery = await new web3.eth.Contract(abi, address);
+  web3.eth.subscribe(eventType, options, eventCallback);
   // lottery = await new web3.eth.Contract(abi)
   //   .deploy({ data: evm.bytecode.object })
   //   .send({ from: accounts[0], gas: '1000000' });
@@ -140,89 +191,91 @@ describe('Lottery Contract', () => {
   it('deploys ethereum contract', () => {
     assert.ok(lottery.options.address);
   });
+  for (let i = 0; i < 3; i++) {
+    // it('allows one account to enter', async () => {
+    //   await lottery.methods.enter().send({
+    //     from: accounts[0],
+    //     value: web3.utils.toWei('0.01', 'ether'),
+    //   });
 
-  // it('allows one account to enter', async () => {
-  //   await lottery.methods.enter().send({
-  //     from: accounts[0],
-  //     value: web3.utils.toWei('0.01', 'ether'),
-  //   });
+    //   const players = await lottery.methods.getPlayers().call({
+    //     from: accounts[0],
+    //   });
 
-  //   const players = await lottery.methods.getPlayers().call({
-  //     from: accounts[0],
-  //   });
-
-  //   assert.equal(accounts[0], players[0]);
-  //   assert.equal(1, players.length);
-  // });
-
-  it('allows multiple accounts to enter', async () => {
-    await lottery.methods.enter().send({
-      from: accounts[0],
-      value: web3.utils.toWei('0.015', 'ether'),
-    });
-    await lottery.methods.enter().send({
-      from: accounts[1],
-      value: web3.utils.toWei('0.006', 'ether'),
-    });
-
-    const players = await lottery.methods.getPlayers().call({
-      from: accounts[0],
-    });
-    console.log(players)
-    // assert.equal(accounts[0], players[0]);
-    // assert.equal(accounts[1], players[1]);
-    // assert.equal(2, players.length);
-  });
-
-  it('requires a minimum amount of ether to enter', async () => {
-    try {
-      await lottery.methods.enter().send({
-        from: accounts[0],
-        value: 0,
-      });
-      assert(false);
-    } catch (err) {
-      assert(err);
-    }
-  });
-
-  it('only manager can call pickWinner', async () => {
-    try {
-      await lottery.methods.pickWinner().send({
-        from: accounts[0],
-      });
-      assert(true);
-    } catch (err) {
-      assert(err);
-    }
-  });
-
-  it('sends money to the winner and resets the players array', async () => {
-    // await lottery.methods.enter().send({
-    //   from: accounts[0],
-    //   value: web3.utils.toWei('0.02', 'ether'),
+    //   assert.equal(accounts[0], players[0]);
+    //   assert.equal(1, players.length);
     // });
 
-    const initialBalance = await web3.eth.getBalance(accounts[0]);
-    // await lottery.methods.pickWinner().send({ from: accounts[0] });
-    const finalBalance = await web3.eth.getBalance(accounts[0]);
-    const difference = finalBalance - initialBalance;
-    // assert(difference > web3.utils.toWei('1.8', 'ether'));
+    it('allows multiple accounts to enter', async () => {
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: web3.utils.toWei('0.015', 'ether'),
+      });
+      await lottery.methods.enter().send({
+        from: accounts[1],
+        value: web3.utils.toWei('0.006', 'ether'),
+      });
 
-    lottery
-    .getPastEvents('RandomReceived', {
-      fromBlock: "3345658",
-      toBlock: 'latest',
-    })
-    .then((events) => {
-      console.log('Events:', events[events.length-1].returnValues);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
+      const players = await lottery.methods.getPlayers().call({
+        from: accounts[0],
+      });
+      console.log(players);
+      // assert.equal(accounts[0], players[0]);
+      // assert.equal(accounts[1], players[1]);
+      // assert.equal(2, players.length);
     });
-  });
+
+    it('requires a minimum amount of ether to enter', async () => {
+      try {
+        await lottery.methods.enter().send({
+          from: accounts[0],
+          value: 0,
+        });
+        assert(false);
+      } catch (err) {
+        assert(err);
+      }
+    });
+
+    it('only manager can call pickWinner', async () => {
+      try {
+        await lottery.methods.pickWinner().send({
+          from: accounts[0],
+          gas: '1000000',
+        });
+        assert(true);
+      } catch (err) {
+        assert(err);
+      }
+    });
+
+    it('sends money to the winner and resets the players array', async () => {
+      // await lottery.methods.enter().send({
+      //   from: accounts[0],
+      //   value: web3.utils.toWei('0.02', 'ether'),
+      // });
+
+      const initialBalance = await web3.eth.getBalance(accounts[0]);
+      // await lottery.methods.pickWinner().send({ from: accounts[0] });
+      const finalBalance = await web3.eth.getBalance(accounts[0]);
+      const difference = finalBalance - initialBalance;
+      // assert(difference > web3.utils.toWei('1.8', 'ether'));
+
+      // lottery
+      // .getPastEvents('RandomReceived', {
+      //   fromBlock: "3345658",
+      //   toBlock: 'latest',
+      // })
+      // .then((events) => {
+      //   console.log('Events:', events[events.length-1].returnValues);
+      // })
+      // .catch((error) => {
+      //   console.error('Error:', error);
+      // });
+    });
+  }
 });
 afterEach((done) => {
   // ...
   done();
-})
+});
