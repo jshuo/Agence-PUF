829ce75a8d8116bb741d0fbe4132516e0489e4ee
Jeff Shuo <jeffshuo@secuxtech.com>
2023-06-14 15:13:12 +0800

    flock implementation

--- puf-fpga-cgi-bin/cgi-bin/run_cgi.sh
+++ puf-fpga-cgi-bin/cgi-bin/run_cgi.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
+########################################################################################
+#
+#  Copyright (c) 2022-2023 PUFsecurity and/or its affiliates. All Rights Reserved.
+#
+#######################################################################################
 
-PORT=8080
+#######################################################################################
+#
+# File: run_cgi.sh
+#
+#       bash script to start busybox httpd
+#
+# History:
+#
+# 2022/11/08  v1.1.0    First release
+# 2023/06/01  v1.2.0    Add flock protection
+#
+#######################################################################################
+
+PORT=80
 CGI_BIN_PARENT_PATH=/home/root/projects/
 
 #busybox httpd -p [port number] -f -v -h [path to the parent directory of `cgi-bin` directory]
